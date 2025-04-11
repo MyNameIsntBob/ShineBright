edc7a4f35a6d60608cb9f14e2115630273a0b723
MyNameIsntBob <37026012+MyNameIsntBob@users.noreply.github.com>
2025-04-11 01:55:30 -0600

    Fix a crash

--- Scripts/Camera2D.gd
+++ Scripts/Camera2D.gd
@@ -14,5 +14,5 @@ func _ready():
 	player2 = get_node(player2_path)
 
 func _process(delta):
-	if player1 and player2:
+	if is_instance_valid(player1) and is_instance_valid(player2):
 		self.position = (player1.global_position + player2.global_position)/2 
